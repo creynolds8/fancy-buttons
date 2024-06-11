9aef6c1220cb71afd56a835156bbaa8f3ce2b3f4
creynolds8 <cameron8.cr@gmail.com>
2024-06-11 15:12:26 -0700

    create click handler function and call on click event

--- src/components/LightSwitchButton.jsx
+++ src/components/LightSwitchButton.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 
 export default function LightSwitchButton() {
   const [ light, setLight ] = useState('off');
+  const handleClick = () => setLight(light === 'off' ? 'on' : 'off')
   return (
-    <button className="LightSwitchButton">
+    <button className="LightSwitchButton" onClick={handleClick}>
       {light === "on" && <span><i>💡</i> I'm on!</span>}
       {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
