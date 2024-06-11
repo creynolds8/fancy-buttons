1dd38113fc6a64bed9e3e1aead705ad4f3705eb1
creynolds8 <cameron8.cr@gmail.com>
2024-06-11 15:37:35 -0700

    lift light state to App and set dark made style to be conditional based on light switch button value

--- src/components/LightSwitchButton.jsx
+++ src/components/LightSwitchButton.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
-export default function LightSwitchButton() {
-  const [ light, setLight ] = useState('off');
-  const handleClick = () => setLight(light === 'off' ? 'on' : 'off')
+export default function LightSwitchButton(props) {
+  // const [ light, setLight ] = useState('off');
+  const handleClick = () => props.setLight(props.light === 'off' ? 'on' : 'off')
+
   return (
     <button className="LightSwitchButton" onClick={handleClick}>
-      {light === "on" && <span><i>💡</i> I'm on!</span>}
-      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
+      {props.light === "on" && <span><i>💡</i> I'm on!</span>}
+      {props.light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
   );
 }
