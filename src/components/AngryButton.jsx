2fc106e5b5e501cf74ceba38e50a79d49db81cad
creynolds8 <cameron8.cr@gmail.com>
2024-06-11 14:19:35 -0700

    import useState into component files

--- src/components/AngryButton.jsx
+++ src/components/AngryButton.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function AngryButton() {
   return (
     <button className="AngryButton">
--- src/components/LightSwitchButton.jsx
+++ src/components/LightSwitchButton.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function LightSwitchButton() {
   return (
     <button className="LightSwitchButton">
