d2bea0cbec53dbceb1d2a89d99c157f0ef1fd58b
creynolds8 <cameron8.cr@gmail.com>
2024-06-11 15:08:08 -0700

    create click handler function and call on click event

--- src/components/AngryButton.jsx
+++ src/components/AngryButton.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 
 export default function AngryButton() {
   const [ anger, setAnger ] = useState(0);
+  const handleClick = () => setAnger(anger + 0.1);
   return (
-    <button style={{ backgroundColor: `rgba(255, 0, 0, ${anger})` }} className="AngryButton">
+    <button style={{ backgroundColor: `rgba(255, 0, 0, ${anger})` }} className="AngryButton" onClick={handleClick}>
       { anger < 1 && <span>Don't click me too much! </span> }
       { anger > 1 && <span>Rawr!</span> }
     </button>
