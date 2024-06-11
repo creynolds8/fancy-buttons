7efef42c3721716515a411328ccf600d142aaacd
creynolds8 <cameron8.cr@gmail.com>
2024-06-11 14:56:13 -0700

    set up conditional rendering for button components

--- src/components/TextRepeaterButton.jsx
+++ src/components/TextRepeaterButton.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
 export default function TextRepeaterButton() {
-  const [ repititions, setRepititions ] = useState();
+  const [ repititions, setRepititions ] = useState(1);
+  const textArr = [];
+  for (let i = 0; i < repititions; i++) {
+    textArr.push(<span key={i}>I like this text</span>);
+  }
+
   return (
     <button className="TextRepeaterButton">
-      <span>I like this text</span>
+      {textArr}
     </button>
   );
 }
