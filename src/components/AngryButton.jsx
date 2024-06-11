bcd3a9d44ff9295cdc6d13ae865eae1a922b8827
creynolds8 <cameron8.cr@gmail.com>
2024-06-11 15:14:17 -0700

    add ternary op to allow button reset

--- src/components/AngryButton.jsx
+++ src/components/AngryButton.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function AngryButton() {
   const [ anger, setAnger ] = useState(0);
-  const handleClick = () => setAnger(anger + 0.1);
+  const handleClick = () => setAnger(anger < 1 ? anger + 0.1 : 0);
   return (
     <button style={{ backgroundColor: `rgba(255, 0, 0, ${anger})` }} className="AngryButton" onClick={handleClick}>
       { anger < 1 && <span>Don't click me too much! </span> }
